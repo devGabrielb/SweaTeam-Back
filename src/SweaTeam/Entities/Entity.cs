0dab26b8af708860ae200867cb7ad8d5a462880b
devGabrielb <dev.gabrielb1@gmail.com>
2024-06-18 00:36:55 -0300

    Start project

---
+++ src/SweaTeam/Entities/Entity.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweaTeam.Entities
+{
+    public abstract class Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        protected Entity(Guid id)
+        {
+            Id = id;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        protected Entity()
+        {
+        }
+
+        public Guid Id { get; init; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public IReadOnlyList<IDomainEvent> GetDomainEvents()
+        {
+            return _domainEvents.ToList();
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+#pragma warning disable CA1030 // Use events where appropriate
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+#pragma warning restore CA1030 // Use events where appropriate
+        {
+            _domainEvents.Add(domainEvent);
+        }
+    }
+}
