e6d9e5b6db21622d590090a77fa7f101ad936dc0
devGabrielb <dev.gabrielb1@gmail.com>
2024-06-22 00:36:44 -0300

    chore: Update program.cs

--- src/SweaTeam/Program.cs
+++ src/SweaTeam/Program.cs
@@ -1,44 +1,45 @@
+using Carter;
+
+using FluentValidation;
+
+using SweaTeam;
+using SweaTeam.extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+#pragma warning disable S125 // Sections of code should not be commented out
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
+    app.ApplyMigrations();
+
+    app.SeedData();
+}
+#pragma warning restore S125 // Sections of code should not be commented out
+
+
+app.MapCarter();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
