790a785f6d6cd294e0683e97a9f4a322d15a8e2a
devGabrielb <dev.gabrielb1@gmail.com>
2024-06-22 00:33:15 -0300

    chore: Add Entity and IDomainEvent classes

---
+++ src/SweaTeam/Common/IDomainEvent.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweaTeam.Common
+{
+#pragma warning disable CA1040 // Avoid empty interfaces
+    public interface IDomainEvent
+#pragma warning restore CA1040 // Avoid empty interfaces
+    {
+
+    }
+}
