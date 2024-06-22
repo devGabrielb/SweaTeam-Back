e59f14450c5048c178355478ee808f5574aabb0b
devGabrielb <dev.gabrielb1@gmail.com>
2024-06-22 00:35:42 -0300

    feat: Add initial migration for Exercise entity

---
+++ src/SweaTeam/Migrations/20240622005039_InitialMigration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SweaTeam.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+#pragma warning disable CA1062 // Validate arguments of public methods
+            migrationBuilder.CreateTable(
+                name: "Exercises",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExrciseMedia = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    PrimaryMuscleGroup = table.Column<int>(type: "integer", nullable: false),
+                    OtherMuscleGroup = table.Column<int[]>(type: "integer[]", nullable: true),
+                    Equipment = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercises", x => x.Id);
+                });
+#pragma warning restore CA1062 // Validate arguments of public methods
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+#pragma warning disable CA1062 // Validate arguments of public methods
+            migrationBuilder.DropTable(
+                name: "Exercises");
+#pragma warning restore CA1062 // Validate arguments of public methods
+        }
+    }
+}
